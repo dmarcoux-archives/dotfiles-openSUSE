9ab6cab780a13850c36796bab1a39257a376c776
Dany Marcoux <danymarcoux@gmail.com>
2018-01-01 21:49:53 +0100

    Adapt emacs config to openSUSE and alter Exec if needed

--- zsh/.config/zsh/emacs.zsh
+++ zsh/.config/zsh/emacs.zsh
@@ -5,5 +5,11 @@ if type emacs > /dev/null; then
   alias emacs='emacs_client.sh'
 
   # Edit emacs' desktop file to launch emacs from an interactive shell, which will expand the alias defined above
-  sudo sed -i 's/Exec=.*/Exec=emacs_client.sh %F/' /usr/share/applications/emacs.desktop
+  if grep -Fxq 'Exec=emacs %F' /usr/share/applications/emacs.desktop; then
+    echo "Provide password to change emacs' Exec in emacs.desktop to use emacsclient."
+    sudo sed -i 's/Exec=.*/Exec=emacs_client.sh %F/' /usr/share/applications/emacs.desktop
+  fi
+
+  # emacs' package on openSUSE installs a default config in the user's directory. It prevent spacemacs from installing.
+  [[ -f ~/.emacs ]] && rm ~/.emacs
 fi
