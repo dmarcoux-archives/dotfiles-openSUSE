c22239562d8fd9c052198531e3d9f2121f6ab79f
Dany Marcoux <danymarcoux@gmail.com>
2018-01-04 23:52:43 +0100

    Setup fzf-marks and load properly zsh completion for fzf

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -37,7 +37,7 @@ if type fzf > /dev/null; then
   # Load default config files (completion and key bindings)
   # Completion at https://github.com/junegunn/fzf#fuzzy-completion-for-bash-and-zsh
   # Key bindings at https://github.com/junegunn/fzf#key-bindings-for-command-line
-  for config in /usr/share/fzf/*.zsh; do source $config; done
+  for config in /etc/zsh_completion.d/fzf{,-key-bindings}; do source $config; done
 
   # fzf-marks: https://github.com/urbainvaes/fzf-marks
   # GNU General Public License v3.0
