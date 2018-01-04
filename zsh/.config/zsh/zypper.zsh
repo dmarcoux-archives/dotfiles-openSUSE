419d833c20ffaa01981dc0dc462d66de034a6820
Dany Marcoux <danymarcoux@gmail.com>
2018-01-04 21:37:48 +0100

    Add details to zrm alias

--- zsh/.config/zsh/zypper.zsh
+++ zsh/.config/zsh/zypper.zsh
@@ -1,4 +1,4 @@
 ealias zif='zypper info --provides --requires --recommends --suggests'
 ealias zin='sudo zypper install --details'
-ealias zrm='sudo zypper remove'
+ealias zrm='sudo zypper remove --details'
 ealias zse='zypper search'
