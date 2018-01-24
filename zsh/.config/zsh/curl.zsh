a2f07e1e3cf76565604b23d3b525c97d2d4a8e7f
Dany Marcoux <danymarcoux@gmail.com>
2018-01-24 10:53:07 +0100

    Add details for --data in curljson alias

--- zsh/.config/zsh/curl.zsh
+++ zsh/.config/zsh/curl.zsh
@@ -1,1 +1,2 @@
-ealias curljson='curl --verbose --header "Content-Type: application/json" --data' # Then add '{"abc":"123","def":"'my text'"}' http://whatever.com
+# --data '{"abc":"123","def":"'my text'"}' or --data @filename (for some file containing JSON)
+ealias curljson='curl --verbose --header "Content-Type: application/json" --data' # See above for --data, then add  http://whatever.com
