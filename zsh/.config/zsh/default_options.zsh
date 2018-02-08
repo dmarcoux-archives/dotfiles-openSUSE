20a2ec372ff09f89c5a3051f4e10fcd9b25a2f2d
Dany Marcoux <danymarcoux@gmail.com>
2018-02-08 15:52:33 +0100

    Add --human-readable to ls defaults

--- zsh/.config/zsh/default_options.zsh
+++ zsh/.config/zsh/default_options.zsh
@@ -1,5 +1,5 @@
 alias history='history 1' # Retrieve all history (useful when using piping the output to grep/ag/whatever)
-alias ls='ls --all --color=auto --format=horizontal --group-directories-first'
+alias ls='ls --all --color=auto --format=horizontal --group-directories-first --human-readable'
 alias mkdir='mkdir --parents --verbose' # No error when directory already exists, more feedback
 alias ping='ping -c 3' # Ping with 3 packets
 alias wget='wget --continue' # Always resume download if partial file found
