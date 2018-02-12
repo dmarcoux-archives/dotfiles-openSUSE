7d1cbdca411b2593c5f451d3d65d2425b9475969
Dany Marcoux <danymarcoux@gmail.com>
2018-02-12 13:28:15 +0100

    Use pass for passwords, not only OTP

--- zsh/.config/zsh/pass.zsh
+++ zsh/.config/zsh/pass.zsh
@@ -3,9 +3,26 @@ if type pass > /dev/null; then
   # Do not pollute the home directory
   export PASSWORD_STORE_DIR=$XDG_CONFIG_HOME/pass/passwords
 
+  # Copy password to clipboard for a specific login
+  ppp(){
+    # List full paths of passwords stored by pass under $PASSWORD_STORE_DIR/pass
+    # Keep the relative paths of these passwords without the .gpg extension (just like how pass does it)
+    # Select one login with fzf
+    # Retrieve the password for the selected login and copy it to the clipboard
+    # TODO: Remove password from clipboard instead of clearing the whole clipboard history (Use CopyQ instead of Parcellite)
+    find $PASSWORD_STORE_DIR/pass -name "*.gpg" -print | sed -e "s|$PASSWORD_STORE_DIR/\(.*\)\.gpg|\1|g" | fzf --no-multi --reverse | xargs --no-run-if-empty pass show --clip
+  }
+
+  # Setup new password
+  # Usage: nppp me/some_website=account
+  # Note: account is the email/username in the login
+  nppp(){
+    pass insert pass/$1
+  }
+
   # Copy two-factor authentication code to clipboard for a specific login
   2fa(){
-    # List full paths of passwords stored by pass under $PASSWORD_STORE_DIR
+    # List full paths of passwords stored by pass under $PASSWORD_STORE_DIR/otp
     # Keep the relative paths of these passwords without the .gpg extension (just like how pass does it)
     # Select one login with fzf
     # Retrieve the OTP code for the selected login and copy it to the clipboard
@@ -13,7 +30,7 @@ if type pass > /dev/null; then
   }
 
   # Setup new two-factor authentication code from a QR code image
-  # Usage: n2fa qr_code.jpg work/github=whatever@email.com
+  # Usage: n2fa qr_code.jpg work/some_website=whatever@email.com
   n2fa(){
     zbarimg -q --raw $1 | pass otp insert otp/$2
   }
