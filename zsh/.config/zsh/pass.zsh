6c93881a882256e7ead5684dac201ec5c4e46a8e
Dany Marcoux <danymarcoux@gmail.com>
2018-02-18 20:43:01 +0100

    Add usage directly in functions of pass.zsh

--- zsh/.config/zsh/pass.zsh
+++ zsh/.config/zsh/pass.zsh
@@ -1,15 +1,23 @@
 # If pass is installed, set it up
 if type pass > /dev/null; then
   # Setup new password
-  # Usage: nppp me/some_website=account
-  # Note: account is the email/username in the login
   nppp(){
+    if [ -z "$1" ]; then
+      # Display usage if no parameters given
+      echo 'Usage: nppp me/some_website=account (account is the email/username in the login)'
+      return
+    fi
+
     pass insert pass/$1
   }
 
   # Setup new two-factor authentication code from a QR code image
-  # Usage: n2fa qr_code.jpg work/some_website=whatever@email.com
   n2fa(){
+    if [ -z "$@" ]; then
+      echo 'Usage: n2fa qr_code.jpg work/some_website=account (account is the email/username in the login)'
+      return
+    fi
+
     zbarimg -q --raw $1 | pass otp insert otp/$2
   }
 fi
