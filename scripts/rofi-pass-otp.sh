c6de197aebe6d684bae626bbb3952fb940a1ff53
Dany Marcoux <danymarcoux@gmail.com>
2018-02-18 16:22:42 +0100

    Type passwords in rofi-pass scripts
    Scripts were also improved following recommendations from ShellCheck

--- scripts/rofi-pass-otp.sh
+++ scripts/rofi-pass-otp.sh
@@ -4,16 +4,16 @@
 #
 # Use with: rofi -modi "pass-otp:/path/to/this_script.sh" -show pass-otp
 
-if [ -z "$@" ]; then
+if [ -z "$*" ]; then
   # rofi first calls the script with no arguments
 
   # List full paths of one-time passwords stored by pass under $PASSWORD_STORE_DIR/otp
   # Keep the relative paths of these one-time passwords without the .gpg extension (just like how pass does it)
-  find $PASSWORD_STORE_DIR/otp -name "*.gpg" -print | sed -e "s|$PASSWORD_STORE_DIR/\(.*\)\.gpg|\1|g"
+  find "$PASSWORD_STORE_DIR/otp" -name "*.gpg" -print | sed -e "s|$PASSWORD_STORE_DIR/\(.*\)\.gpg|\1|g"
 else
   # Once the user selected an entry, rofi calls the script again with it as an argument
 
   # Retrieve the one-time password for the selected login and copy it to the clipboard
-  # Note: coproc starts the command as a background job (so it doesn't freeze rofi)
-  coproc (pass otp --clip "$@" > /dev/null &)
+  # Note: coproc starts the command group ({ ... }) as a background job (so it doesn't freeze rofi)
+  coproc { pass otp "$*" | xdotool type --clearmodifiers --file - > /dev/null & }
 fi
--- scripts/rofi-pass.sh
+++ scripts/rofi-pass.sh
@@ -4,17 +4,16 @@
 #
 # Use with: rofi -modi "pass:/path/to/this_script.sh" -show pass
 
-if [ -z "$@" ]; then
+if [ -z "$*" ]; then
   # rofi first calls the script with no arguments
 
   # List full paths of passwords stored by pass under $PASSWORD_STORE_DIR/pass
   # Keep the relative paths of these passwords without the .gpg extension (just like how pass does it)
-  find $PASSWORD_STORE_DIR/pass -name "*.gpg" -print | sed -e "s|$PASSWORD_STORE_DIR/\(.*\)\.gpg|\1|g"
+  find "$PASSWORD_STORE_DIR/pass" -name "*.gpg" -print | sed -e "s|$PASSWORD_STORE_DIR/\(.*\)\.gpg|\1|g"
 else
   # Once the user selected a password, rofi calls the script again with the selected password as an argument
 
   # Retrieve the password for the selected login and copy it to the clipboard
-  # TODO: Remove password from clipboard instead of clearing the whole clipboard history (Use CopyQ instead of Parcellite)
-  # Note: coproc starts the command as a background job (so it doesn't freeze rofi)
-  coproc (pass show --clip "$@" > /dev/null &)
+  # Note: coproc starts the command group ({ ... }) as a background job (so it doesn't freeze rofi)
+  coproc { pass show "$*" | xdotool type --clearmodifiers --file - > /dev/null & }
 fi
