b7f98075191d77ff4b62e9ca3389da4a74487fc6
Dany Marcoux <danymarcoux@gmail.com>
2018-03-09 12:57:21 +0100

    Generate list of packages in backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,3 +5,8 @@
 # -u: Exit on unset variables
 # -x: Write to standard error a trace for each command after it expands the command and before it executes it
 set -eux
+
+mkdir --parents "$HOME/dotfiles/backup/$HOSTNAME"
+
+# Generate sorted list of explicitly installed packages
+zypper search --installed-only | tr --delete " " | sed -nr '/^(i\+)/p' | cut --delimiter="|" --fields=2 | sort > "$HOME/dotfiles/backup/$HOSTNAME/packages.txt"
