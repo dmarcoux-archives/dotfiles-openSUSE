e666a3363bcfc5255897c45ea677f3c40dc69a0e
Dany Marcoux <danymarcoux@gmail.com>
2018-04-05 14:34:11 +0200

    Start rubocop setup

--- zsh/.config/zsh/ruby.zsh
+++ zsh/.config/zsh/ruby.zsh
@@ -1,4 +1,13 @@
 # If rbenv is installed, set it up
 if type rbenv > /dev/null 2>&1; then
   eval "$(rbenv init -)"
+
+  # TODO: Make sure rbenv is not using the system ruby
+  # Install rubocop if not already installed
+  if ! type rubocop > /dev/null 2>&1; then
+    # TODO: Update this automatically with a PR in my dotfiles' repository when the following file changes:
+    #       Look into using hub (https://hub.github.com/) to create the PR from the command line after changing the files
+    # https://github.com/bbatsov/rubocop/blob/master/lib/rubocop/version.rb
+    gem install --no-document rubocop --version 0.54.0
+  fi
 fi
