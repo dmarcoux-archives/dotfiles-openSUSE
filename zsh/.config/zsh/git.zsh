bf3d1aca41215ff42bf8d2717ca65ef68d64ec6c
Dany Marcoux <danymarcoux@gmail.com>
2018-04-09 23:54:03 +0200

    Setup hub and create fork functions

--- zsh/.config/zsh/git.zsh
+++ zsh/.config/zsh/git.zsh
@@ -70,3 +70,47 @@ add_repo() {
   echo 'git log'
   echo 'git push'
 }
+
+# If hub is installed, set it up
+if type hub > /dev/null 2>&1; then
+  GITHUB_USER="dmarcoux"
+
+  # Fork a Git repository. It clones the repository, creates the fork on GitHub and sets remotes
+  fork_repo() {
+    if [ -z "$1" ]; then
+      # Display usage
+      echo "Usage: fork_repo git@github.com:username/repo.git"
+      return
+    fi
+
+    REPOSITORY_URL="$1"
+    REPOSITORY_NAME="$(basename "$1" .git)"
+
+    # Clone repository
+    git clone "$REPOSITORY_URL"
+
+    # Go into cloned repository and fork it
+    (
+      cd "$REPOSITORY_NAME" || exit
+
+      # Pass GITHUB_TOKEN with personal access token to avoid storing GitHub credentials ('man hub' for details)
+      # Generate a personal access token at https://github.com/settings/tokens/new
+      GITHUB_TOKEN="$(pass pass/me/hub-access-token)" hub fork
+
+      # Rename remotes created by hub
+      # - origin (repository) is renamed to upstream
+      # - my_user_name (fork) is renamed to origin
+      git remote rename origin upstream
+      git remote rename "$GITHUB_USER" origin
+    )
+  }
+
+  # Update fork of a Git repository (it has to be setup with fork_repo to follow the remote naming convention)
+  update_fork() {
+    # Rebase master on the upstream changes
+    git pull --rebase --autostash upstream master
+
+    # Push updated master to my fork
+    git push origin master
+  }
+fi
