1cb6a1d73c0013cb3fae384ba4afadc8b43cb1ae
Dany Marcoux <danymarcoux@gmail.com>
2018-04-09 21:45:47 +0200

    Setup aliases for osc

---
+++ zsh/.config/zsh/osc.zsh
@@ -0,0 +1,32 @@
+# If osc is installed, set it up
+if type osc > /dev/null 2>&1; then
+  # Mark {new,deleted} local files to be {added,deleted}
+  ealias oar='osc addremove'
+
+  # Mark file(s) to be added upon next commit
+  ealias oa='osc add'
+
+  # Mark file(s) to be deleted upon next commit
+  ealias orm='osc rm'
+
+  # Show status of files
+  ealias ost='osc st'
+
+  # Edit the changes file
+  ealias ovc='osc vc'
+
+  # Push changes to remote
+  ealias oci='osc ci'
+
+  # Select one of the configured repositories for the project and build the project for it
+  ealias obf='osc repos | fzf | tr --squeeze-repeats " " | cut --delimiter=" " --fields=1 | xargs --no-run-if-empty osc build --target='
+
+  # Build the project for Tumbleweed
+  ealias obt='osc build --target=openSUSE_Tumbleweed/x86_64'
+
+  # Build the project for Leap 15.0
+  ealias obl='osc build --target=openSUSE_Leap_15.0/x86_64'
+
+  # Download files referenced via source URL in the project's spec file
+  ealias odl='osc service localrun download_files'
+fi
