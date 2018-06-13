11a9d3644861ab32b8a37bf82b3cb09e277e5b6c
Dany Marcoux <danymarcoux@gmail.com>
2018-06-13 12:13:19 +0200

    Add fzf config to package changes

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -3,7 +3,7 @@ if type fzf > /dev/null 2>&1; then
   # Load default config files (completion and key bindings)
   # Completion at https://github.com/junegunn/fzf#fuzzy-completion-for-bash-and-zsh
   # Key bindings at https://github.com/junegunn/fzf#key-bindings-for-command-line
-  for config in /etc/zsh_completion.d/fzf{,-key-bindings}; do source $config; done
+  source /etc/zsh_completion.d/fzf-key-bindings
 
   # fzf-marks: https://github.com/urbainvaes/fzf-marks
   # GNU General Public License v3.0
