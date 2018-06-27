0b15d8896f1cb54b13e9761be81574948b144e8a
Dany Marcoux <danymarcoux@gmail.com>
2018-06-27 19:35:51 +0200

    Source fzf completion in zsh

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -3,6 +3,7 @@ if type fzf > /dev/null 2>&1; then
   # Load default config files (completion and key bindings)
   # Completion at https://github.com/junegunn/fzf#fuzzy-completion-for-bash-and-zsh
   # Key bindings at https://github.com/junegunn/fzf#key-bindings-for-command-line
+  source /usr/share/zsh/site-functions/_fzf
   source /etc/zsh_completion.d/fzf-key-bindings
 
   # TODO: Fix minor bug which doesn't display VCS info
