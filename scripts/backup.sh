9972fff7177996fc1f1bd78ed06310f224b18605
Dany Marcoux <danymarcoux@gmail.com>
2018-06-30 19:47:02 +0200

    Adapt backup.sh for SSH/GPG keys

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,7 +6,33 @@
 # -x: Write to standard error a trace for each command after it expands the command and before it executes it
 set -eux
 
-mkdir --parents "$HOME/dotfiles/backup/$HOSTNAME"
+BACKUP_DIR="$HOME/dotfiles/backup/$HOSTNAME/keys"
 
-# Generate sorted list of explicitly installed packages
-zypper search --installed-only | tr --delete " " | sed -nr '/^(i\+)/p' | cut --delimiter="|" --fields=2 | sort > "$HOME/dotfiles/backup/$HOSTNAME/packages.txt"
+mkdir --parents "$BACKUP_DIR"
+
+echo 'Backup GPG and SSH keys?'
+select choice in "Yes" "No"; do
+  case $choice in
+    Yes ) echo 'Exporting GPG keys and their trusts'
+          gpg --export-secret-keys --armor --output "$BACKUP_DIR/secrets.asc"
+          gpg --export-ownertrust > "$BACKUP_DIR/otrust.txt"
+
+          echo 'Exporting SSH keys'
+          cp ~/.ssh/*id_rsa* "$BACKUP_DIR"
+
+          echo 'Archiving keys'
+          (cd "$BACKUP_DIR" && tar --create --file=keys.tar secrets.asc otrust.txt ./*id_rsa*)
+
+          echo 'Encrypting the tar archive'
+          (cd "$BACKUP_DIR" && gpg --symmetric --cipher-algo=AES256 keys.tar)
+
+          echo 'Removing exported GPG/SSH keys and the unencrypted tar archive'
+          (cd "$BACKUP_DIR" && rm secrets.asc otrust.txt ./*id_rsa* keys.tar)
+
+          echo 'Reloading GPG agent to clear the cached passphrase'
+          gpg-connect-agent reloadagent /bye 1> /dev/null
+
+          break ;;
+    No ) break ;;
+  esac
+done
