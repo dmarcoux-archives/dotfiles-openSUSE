fd640e0aba82a881fa94403335e8507a812bf04e
Dany Marcoux <danymarcoux@gmail.com>
2018-07-25 23:50:21 +0200

    Remove extract function in zsh
    I learned about atool and aunpack, which is much better than my silly
    script

--- zsh/.config/zsh/general.zsh
+++ zsh/.config/zsh/general.zsh
@@ -27,45 +27,6 @@ upc() {
   # Goes one directory up, then cd into directory (if found, otherwise list directories with fzf to select one)
 }
 
-# Extract archives
-extract() {
-  if [ -z "$1" ]; then
-    # Display usage if no parameters given
-    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
-    return
-  fi
-
-  for file in "$@"
-  do
-    if [ -f "$file" ]; then
-      NAME=${file%.*}
-      mkdir $NAME && cd $NAME
-      case $file in
-        *.tar.bz2)   tar xvjf ../$file     ;;
-        *.tar.gz)    tar xvzf ../$file     ;;
-        *.tar.xz)    tar xvJf ../$file     ;;
-        *.lzma)      unlzma ../$file       ;;
-        *.bz2)       bunzip2 ../$file      ;;
-        *.rar)       unrar x -ad ../$file  ;;
-        *.gz)        gunzip ../$file       ;;
-        *.tar)       tar xvf ../$file      ;;
-        *.tbz2)      tar xvjf ../$file     ;;
-        *.tgz)       tar xvzf ../$file     ;;
-        *.zip)       unzip ../$file        ;;
-        *.Z)         uncompress ../$file   ;;
-        *.7z)        7z x ../$file         ;;
-        *.xz)        unxz ../$file         ;;
-        *.exe)       cabextract ../$file   ;;
-        *)           echo "extract: '$file ' - unknown archive method" ;;
-      esac
-
-      cd ..
-    else
-      echo "$file  - file does not exist"
-    fi
-  done
-}
-
 # Export environment variables from a file (by default .env, but can be passed as an argument)
 exportenv() {
   FILE=${1-.env}
