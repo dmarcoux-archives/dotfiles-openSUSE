8d7e5c5274d3fa509d466db7a103e8d8307aa56b
Dany Marcoux <danymarcoux@gmail.com>
2018-08-12 09:36:08 +0200

    Use function instead of alias for Weechat
    The alias triggers pass on startup, which is then displaying the gpg
    pinentry. I want to avoid this

--- zsh/.config/zsh/weechat.zsh
+++ zsh/.config/zsh/weechat.zsh
@@ -1,4 +1,7 @@
 # If weechat is installed, set it up
 if type weechat > /dev/null 2>&1; then
-  alias weechat="WEECHAT_PASSPHRASE="$(pass pass/me/weechat_passphrase)" weechat"
+  weechat() {
+    export WEECHAT_PASSPHRASE="$(pass pass/me/weechat_passphrase)"
+    command weechat
+  }
 fi
