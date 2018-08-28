4b791743004270214c436541ab9bd18bbaa810f0
Dany Marcoux <danymarcoux@gmail.com>
2018-08-28 23:54:12 +0200

    Start configuring weechat with inwee

--- zsh/.config/zsh/weechat.zsh
+++ zsh/.config/zsh/weechat.zsh
@@ -7,8 +7,18 @@ if type weechat > /dev/null 2>&1; then
 
   # If inwee is installed, set it up
   if type inwee > /dev/null 2>&1; then
+    # Set default options for inwee
+    inwee() {
+      command inwee --fifo "$WEECHAT_HOME"/weechat_fifo
+    }
+
     configure_weechat() {
-      inwee "$XDG_CONFIG_HOME"/weechat/settings.inwee
+      # TODO: Finish this
+      # Settings which need to be expanded before being passed to inwee
+      echo "/secure set freenode_password "$(pass pass/me/freenode.net=dmarcoux)"" | inwee
+
+      # All settings without expansion are in settings.inwee
+      inwee "$WEECHAT_HOME"/settings.inwee
     }
 
     # Send text/commands to Weechat buffer
