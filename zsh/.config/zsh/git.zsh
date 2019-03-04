697ade6342f7d15dc79eeaeb43fa28a2a6346ac8
Dany Marcoux <danymarcoux@gmail.com>
2019-03-05 00:37:16 +0100

    Use xargs in grebi alias

--- zsh/.config/zsh/git.zsh
+++ zsh/.config/zsh/git.zsh
@@ -36,8 +36,7 @@ ealias gpure='git pull --rebase --autostash'
 ealias greb='git rebase'
 ealias grebc='git rebase --continue'
 # From the output of the glo alias, select with fzf the commit hash to rebase on
-# TODO: Improve this to use xargs. It's not possible right now as xargs on Linux doesn't support -o, which prevent Vim from complaining about the input
-alias grebi='git rebase --interactive $(glo | fzf --ansi --no-sort | cut --delimiter=" " --fields=1)'
+alias grebi='glo | fzf --ansi --no-sort | cut --delimiter=" " --fields=1 | xargs --no-run-if-empty -I % bash -c "</dev/tty git rebase --interactive %"'
 ealias gref='git reflog' # then: git reset HEAD@{index}
 ealias gres='git reset'
 ealias grev='git revert'
