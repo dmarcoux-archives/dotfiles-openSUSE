8376fdfadbec46743ecd6d9aa79b7192770eecf4
Dany Marcoux <danymarcoux@gmail.com>
2019-08-18 23:23:14 +0200

    Check if npm is installed before installing npm packages

--- zsh/.config/zsh/node.zsh
+++ zsh/.config/zsh/node.zsh
@@ -3,22 +3,25 @@ export N_PREFIX="$HOME/n"
 
 [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
 
-# If TypeScript compiler is not installed, install it
-if ! type tsc > /dev/null 2>&1; then
-  npm install -g typescript
-fi
+# If npm is installed, then set it up
+if type npm > /dev/null 2>&1; then
+  # If TypeScript compiler is not installed, install it
+  if ! type tsc > /dev/null 2>&1; then
+    npm install -g typescript
+  fi
 
-# If TypeScript linter is not installed, install it
-if ! type tslint > /dev/null 2>&1; then
-  npm install -g tslint
-fi
+  # If TypeScript linter is not installed, install it
+  if ! type tslint > /dev/null 2>&1; then
+    npm install -g tslint
+  fi
 
-# If TypeScript formatter is not installed, install it
-if ! type tsfmt > /dev/null 2>&1; then
-  npm install -g typescript-formatter
-fi
+  # If TypeScript formatter is not installed, install it
+  if ! type tsfmt > /dev/null 2>&1; then
+    npm install -g typescript-formatter
+  fi
 
-# If tern (editor-independent Javascript analyzer) is not installed, install it
-if ! type tern > /dev/null 2>&1; then
-  npm install -g tern
+  # If tern (editor-independent Javascript analyzer) is not installed, install it
+  if ! type tern > /dev/null 2>&1; then
+    npm install -g tern
+  fi
 fi
