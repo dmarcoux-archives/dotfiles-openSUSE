978d562396ce89384e2ef538938e54b57baf622c
Dany Marcoux <danymarcoux@gmail.com>
2019-08-18 23:10:33 +0200

    Set Konsole's tab bar position

--- scripts/kde/setup_konsole.sh
+++ scripts/kde/setup_konsole.sh
@@ -2,3 +2,5 @@
 
 # Set Konsole's default profile to SolarizedLight.profile (it should be stowed beforehand)
 kwriteconfig5 --file konsolerc --group 'Desktop Entry' --key 'DefaultProfile' 'SolarizedLight.profile'
+
+kwriteconfig5 --file konsolerc --group 'TabBar' --key 'TabBarPosition' 'Top'
