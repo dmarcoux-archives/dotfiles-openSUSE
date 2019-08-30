39c47475e9147a237c653d202bd81e586665551b
Dany Marcoux <danymarcoux@gmail.com>
2019-08-30 14:29:43 +0200

    Remove alias for bumblebee
    I don't use bumblebee anymore.

--- zsh/.config/zsh/bumblebee.zsh
+++
@@ -1,4 +0,0 @@
-# If bumblebee is supported, optirun/primusrun should be installed, setup aliases
-if type optirun > /dev/null 2>&1; then
-  alias nvidia-settings='optirun -b none nvidia-settings -c :8'
-fi
