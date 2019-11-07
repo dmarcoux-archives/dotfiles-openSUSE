c652aa5e3a7206d3212e08a8510b1a70f3ff6f6a
Dany Marcoux <dmarcoux@posteo.de>
2019-11-07 13:14:35 +0100

    Remove most language-specific configurations
    I haven't used any of those for at least a few months, if not a year. The
    only exception is for Node, since I still need to find a way around this. One
    possible solution might be `Remote Development[0]` in VS Code.
    
    [0] https://code.visualstudio.com/docs/remote/containers

--- zsh/.config/zsh/rust.zsh
+++
@@ -1,5 +0,0 @@
-# If cargo is installed, set it up
-if type cargo > /dev/null 2>&1; then
-  ealias cb='cargo build'
-  ealias cr='cargo run'
-fi
--- zsh/.config/zsh/stack.zsh
+++
@@ -1,4 +0,0 @@
-# If stack is installed, set it up
-if type stack > /dev/null 2>&1; then
-  eval "$(stack --bash-completion-script stack)"
-fi
